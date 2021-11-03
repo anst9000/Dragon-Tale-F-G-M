4cd6396367927a57fe6445e0d2ca33acb1d85bee
Anders Strömberg <coltlaboy@yahoo.se>
2021-11-03 09:46:03 +0100

    Game is done and played

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -4,13 +4,15 @@ import javax.swing.JFrame;
 
 public class Game
 {
-	public static void main( String[ ] args )
+
+	public static void main( String[] args )
 	{
+
 		JFrame window = new JFrame( "Dragon Tale" );
-		window.setContentPane( new GamePanel( ) );
+		window.setContentPane( new GamePanel() );
 		window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		window.setResizable( false );
-		window.pack( );
+		window.pack();
 		window.setVisible( true );
 	}
 }
